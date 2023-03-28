0d46468590ab524a693b0d85d0a6e7d290f7fbad
Короче, Максон <102228515+HackFoxYeah@users.noreply.github.com>
2023-03-28 12:19:15 +0300

    Add files via upload

---
+++ DataBaseSupClass.cs
@@ -0,0 +1,13 @@
+﻿namespace kapustinRPMBD
+{
+    public static class DataBaseSupClass
+    {
+        public static int flightNumber;
+        public static string destination;
+        public static System.TimeSpan departureTime;
+        public static System.TimeSpan arrivalTime;
+        public static int availableSeatsCount;
+        public static string aircraftType;
+        public static int capacity;
+    }
+}
